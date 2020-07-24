23d1374a0e3d0a01d0e29873b650fdf174e69d12
EscapedSheep <sfgmdf001@126.com>
2020-07-24 20:52:59 +0800

    [maida Ge] Complted GrammerExerciseTest

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -12,7 +16,20 @@ public class GrammarExercise {
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+        firstWordList = firstWordList.toUpperCase();
+        secondWordList = secondWordList.toUpperCase();
+        List<String> firstList = Arrays.asList(firstWordList.split(","));
+        List<String> secondList = Arrays.asList(secondWordList.split(","));
+        Predicate<String> isWord = s -> s.matches("^[a-zA-Z]+$");
+        if (!firstList.stream().allMatch(isWord) || !secondList.stream().allMatch(isWord)) {
+            throw new RuntimeException();
+        };
+        return firstList
+                .stream()
+                .distinct()
+                .filter(s -> secondList.contains(s))
+                .map(s -> String.join(" ",s.split("")))
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
